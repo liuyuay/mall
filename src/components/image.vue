e61746c241b15852d26d144a3cae8e5aef50abcb
liuyuay <30967086@qq.com>
2020-08-10 11:06:22 +0800

    liuyuay

--- src/components/image.vue
+++ src/components/image.vue
@@ -1,22 +1,36 @@
 <template>
-  <div class="demo-image__preview">
-    <el-image
-      style="width: 100px; height: 100px"
-      :src="url"
-      :preview-src-list="srcList">
-    </el-image>
-  </div>
+<!--  <div class="demo-image__preview">-->
+<!--    <el-image-->
+<!--      style="width: 100px; height: 100px"-->
+<!--      :src="url"-->
+<!--      :preview-src-list="srcList">-->
+<!--    </el-image>-->
+<!--  </div>-->
+  <div
+    class="markdown-body"
+    v-html="content" align="left"/>
 </template>
-
 <script>
+import 'mavon-editor/dist/css/index.css'
 export default {
+  // data () {
+  //   return {
+  //     url: '../static/img/avatar.jpeg',
+  //     srcList: [
+  //       '../static/image/a.jpg',
+  //       '../static/image/b.jpg'
+  //     ]
+  //   }
+  // }
   data () {
     return {
-      url: '../static/img/avatar.jpeg',
-      srcList: [
-        '../static/image/a.jpg',
-        '../static/image/b.jpg'
-      ]
+      content: '<blockquote>\n' +
+        '<p>欢迎使用Markdown编辑器<br />\n' +
+        'liuyuay.cn</p>\n' +
+        '</blockquote>\n' +
+        '<ul>\n' +
+        '<li>KKJFKLDJL</li>\n' +
+        '</ul>'
     }
   }
 }
